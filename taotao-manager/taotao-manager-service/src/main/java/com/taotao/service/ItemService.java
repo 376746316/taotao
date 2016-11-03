27ae26f798c29e32368e0aa5d88cf95a1fec808e
DADI-20160501NK\Administrator <376746316@qq.com>
2016-11-04 01:01:44 +0800

    商品添加功能

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.taotao.service;
 
 import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.pojo.TbItem;
 
 public interface ItemService {
@@ -8,4 +9,6 @@ public interface ItemService {
 	public TbItem getItemById(Long id);
 	
 	public EasyUIDataGridResult getItemListByPage(int page,int rows);
+	
+	TaotaoResult addItem(TbItem item);
 }
--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -4,29 +4,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 
 @Controller
 public class ItemController {
 
-	@Autowired 
+	@Autowired
 	private ItemService itemService;
-	
+
 	@RequestMapping("/item/{itemId}")
-	public @ResponseBody TbItem getItemById(@PathVariable Long itemId){
-		
+	public @ResponseBody TbItem getItemById(@PathVariable Long itemId) {
+
 		return itemService.getItemById(itemId);
-		
+
 	}
-	
+
 	@RequestMapping("/item/list")
-	public @ResponseBody EasyUIDataGridResult getItemList(int page,int rows){
+	public @ResponseBody EasyUIDataGridResult getItemList(int page, int rows) {
 		return itemService.getItemListByPage(page, rows);
 	}
-	
-	
+
+	@RequestMapping(value = "/item/save", method = RequestMethod.POST)
+	public @ResponseBody TaotaoResult addItem(TbItem item) {
+
+		return itemService.addItem(item);
+	}
+
 }
